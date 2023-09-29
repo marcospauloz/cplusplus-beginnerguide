2ecf4cdf874ac3128802c6012f0a56f06e819cf4
Marcos Paulo <marcospaulozanelatto@live.com>
2023-09-29 07:11:45 -0300

    Working with functions and collections

---
+++ Functions/Functions.h
@@ -0,0 +1,4 @@
+#pragma once
+
+double add(double x, double y);
+double add(double a, double b, double c);
---
+++ Functions/Program.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using std::cout;
+
+#include "Functions.h"
+
+int main()
+{
+	double total = add(3, 4);
+	cout << "3 + 4 is " << total << '\n';
+
+	double another = add(1.2, 1.4);
+	cout << "1.2 + 1.4 is " << another << '\n';
+
+	double totalofthree = add(1.1, 2.2, 3.3);
+	cout << "1.1 + 2.2, 3.3 is " << totalofthree << '\n';
+	
+	return 0;
+}
